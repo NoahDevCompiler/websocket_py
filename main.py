66eb87dceaa0188c277fbcef99f70ee600b57c39
Noah <Noah@DAS-LAPTOP>
2025-05-12 16:43:58 +0200

    Changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import websockets
 import os
 import subprocess
 import platform
+import ssl
 
 def exec_command(command):
     if command == 'open_calc':
@@ -14,17 +15,18 @@ def exec_command(command):
         elif platform.system() == "Linux" or platform.system() == "Darwin":
             os.system("shutdown -h now")
 
-async def handler(websocket):
+async def handler(websocket, path):
     async for message in websocket:
         print(f"Empfangene Nachricht: {message}")
         exec_command(message)
 
 async def main():
-    async with websockets.serve(handler, "0.0.0.0", 10000):
-        print("Se4ver Runnnig")
-        await asyncio.Future()
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile="server.crt", keyfile="private.pem")
+
+    server = await websockets.serve(handler, "0.0.0.0", 10000, ssl=ssl_context)
+    print("Server läuft auf wss://localhost:10000")
+    await server.wait_closed()
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-    
